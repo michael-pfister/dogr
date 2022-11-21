1cf3e48a9964dc4d8ecb7bcabb9099465579c19c
stracciatella27 <schoko.bombe44@gmail.com>
2022-11-21 17:25:34 +0100

    initialize

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-screen h-screen flex justify-center items-center gap-4">
+    <NuxtLogo class="w-16" />
+    <h1 class="text-5xl">Setup Complete</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
