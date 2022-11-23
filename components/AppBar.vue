0c18afad72222658b3b17ae4dc11264fcba5a8b7
stracciatella27 <schoko.bombe44@gmail.com>
2022-11-23 12:59:52 +0100

    change workstation

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>AppBar</div>
+  <header>
+    <nuxt-link class="flex gap-2 items-center h-12" to="/">
+      <NuxtLogo class="w-8" />
+      <h3>Dogr.</h3>
+    </nuxt-link>
+    <nav>
+      <ul class="flex gap-4 text-xl">
+        <li>
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/contact">Contact</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
