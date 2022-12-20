3aeb8a13867cf9c3eb0ffd233889b9d5f34b0074
stracciatella27 <schoko.bombe44@gmail.com>
2022-12-20 19:19:51 +0100

    add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="flex justify-center items-center flex-wrap p-2">
+    <img src="~/assets/img/404 Page Not Found _Outline.svg" alt="Error" />
+    <div class="flex flex-col items-center gap-6">
+      <h1>Oops!</h1>
+      <p v-if="error.statusCode === 404">
+        We couldn't find the page you were looking for.
+      </p>
+      <p v-else>An error occurred.</p>
+      <NuxtLink to="/" class="bg-green p-2 rounded-full text-black"
+        >Return Home</NuxtLink
+      >
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
