9fba3beb5a9581580ef427b9444ef11c2a59e60d
stracciatella27 <schoko.bombe44@gmail.com>
2023-01-02 20:47:41 +0100

    add link sharing page

--- pages/mydogs.vue
+++ pages/mydogs.vue
@@ -14,13 +14,32 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import Pusher from 'pusher-js'
 
 export default defineComponent({
   transition: 'slide',
   setup() {
     const myDogs = ref([])
 
-    return { myDogs }
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('0bd97ca688d0795c80e3', {
+      cluster: 'eu',
+    })
+
+    const channel = pusher.subscribe('chat');
+
+    /* channel.bind('my-event', function(data: {}) {
+      alert(JSON.stringify(data));
+    }); */
+
+    // works
+    /* channel.emit('my-event', {
+      message: 'hello world'
+    }); */
+
+    return { myDogs, pusher, channel }
   },
   mounted() {
     // Check for myDogs localStorage
@@ -28,6 +47,18 @@ export default defineComponent({
 
     // Get myDogs from localStorage
     this.myDogs = JSON.parse(localStorage.getItem('myDogs') || '[]')
+
+    /* --- Pusher --- */
+
+    // Check for successful subscription
+    this.channel.bind('pusher:subscription_succeeded', () => {
+      console.log('Successfully connected to Pusher')
+    })
+
+    // Check for errors
+    this.channel.bind('pusher:subscription_error', (status: {}) => {
+      console.log('Error connecting to Pusher', status)
+    })
   },
   methods: {
     checkForMyDogsStorage() {
