47368fef1481aabd3cc39df4ef4aee078e9ec4b7
stracciatella27 <schoko.bombe44@gmail.com>
2023-01-11 17:01:57 +0100

    fix SSR bug

--- pages/dogs.vue
+++ pages/dogs.vue
@@ -1,24 +1,27 @@
 <template>
-  <section class="flex flex-col items-center">
-    <div class="flex gap-2">
-      <h2 class="text-center">
-        <text class="text-green">Wow!</text> These are some awesome
-      </h2>
-      <h2 class="animate-spin">🐶</h2>
-      <h2>!</h2>
-    </div>
-    <ul v-if="images.length" class="dogList my-8">
-      <li v-for="(image, index) in images" :key="`dog_${index}`">
-        <ImageDisplay :picture="image" />
-      </li>
-    </ul>
-    <p v-else class="text-center my-16">
-      Oops! Seems like your link got corrupted.
-    </p>
-    <NuxtLink to="/" class="bg-green p-2 rounded-full text-black"
-      >Return Home</NuxtLink
-    >
-  </section>
+  <!-- fixes SSR bug, do not remove -->
+  <client-only>
+    <section class="flex flex-col items-center">
+      <div class="flex gap-2">
+        <h2 class="text-center">
+          <text class="text-green">Wow!</text> These are some awesome
+        </h2>
+        <h2 class="animate-spin">🐶</h2>
+        <h2>!</h2>
+      </div>
+      <ul v-if="images.length" class="dogList my-8">
+        <li v-for="(image, index) in images" :key="`dog_${index}`">
+          <ImageDisplay :picture="image" />
+        </li>
+      </ul>
+      <p v-else class="text-center my-16">
+        Oops! Seems like your link got corrupted.
+      </p>
+      <NuxtLink to="/" class="bg-green p-2 rounded-full text-black"
+        >Return Home</NuxtLink
+      >
+    </section>
+  </client-only>
 </template>
 
 <script lang="ts">
@@ -39,14 +42,10 @@ export default defineComponent({
       images = []
     }
 
-    console.log('here')
-
     // filter out any non-image urls
     images = images
       ? (images as string[]).filter((url: string) => isImage(url))
       : []
-
-    console.log('images')
 
     return { images }
   },
