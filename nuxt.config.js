fda199b49a494535c05113eaca4921cca1a7064a
stracciatella27 <schoko.bombe44@gmail.com>
2023-01-16 13:08:28 +0100

    add vercel.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 export default {
+  mode: 'universal',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Dogr. | Find Pictures of Any Dog Breed',
@@ -55,6 +56,4 @@ export default {
       },
     },
   },
-
-  target: 'static',
 }
